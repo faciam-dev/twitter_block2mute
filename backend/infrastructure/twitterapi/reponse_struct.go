a4de0d54e5bfc0d614924a432cce8a5b3be549ab
Masami Murai <masami_murai@faciam.net>
2022-10-08 21:06:58 +0900

    [modify] anacondaのハンドラの削除

--- backend/infrastructure/twitterapi/reponse_struct.go
+++ backend/infrastructure/twitterapi/reponse_struct.go
@@ -1,5 +1,46 @@
 package twitterapi
 
+import (
+	"net/url"
+
+	"github.com/garyburd/go-oauth/oauth"
+)
+
+// TwitterCredentials
+type TwitterCredentials struct {
+	Credentials *oauth.Credentials
+}
+
+func (t TwitterCredentials) GetToken() string {
+	return t.Credentials.Token
+}
+
+func (t TwitterCredentials) GetSecret() string {
+	return t.Credentials.Secret
+}
+
+// TwitterValues
+type TwitterValues struct {
+	Vaules url.Values
+}
+
+func (t TwitterValues) GetTwitterID() string {
+	return t.getTwitterValue("user_id")
+}
+
+func (t TwitterValues) GetTwitterScreenName() string {
+	return t.getTwitterValue("screen_name")
+}
+
+func (t TwitterValues) getTwitterValue(key string) string {
+	for k, v := range t.Vaules {
+		if k == key {
+			return v[0]
+		}
+	}
+	return ""
+}
+
 // TwitterUser
 type TwitterUser struct {
 	ID          string
